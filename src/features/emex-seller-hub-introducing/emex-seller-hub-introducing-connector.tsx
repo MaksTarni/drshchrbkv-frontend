cee95e6bf637b0205a8934c0aae19f0675b2d951
FVS <fvs@ip-10-0-0-2.eu-west-1.compute.internal>
2026-03-05 23:31:58 +0200

    chore: удалил неиспользуемое

--- src/features/emex-seller-hub-introducing/emex-seller-hub-introducing-connector.tsx
+++ src/features/emex-seller-hub-introducing/emex-seller-hub-introducing-connector.tsx
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 import { EmexSellerHubIntroducing } from './emex-seller-hub-introducing';
 import { mapDataToUI } from './utils';
 import { useEmexSellerHubIntro } from '../../entities/emex-seller-hub-intro';
-import { resolvedImagesByDimensions } from './utils/resolved-images-by-dimensions';
 import { resolveImageByBreakpoint } from '../../shared/utils';
 import { useDimensions } from '../../shared/hooks/dimensions';
 
