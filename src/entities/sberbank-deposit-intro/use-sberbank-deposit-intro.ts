30152bace58565a15a6ed476ae33c4f5789faaba
FVS <fvs@MacBook-Pro-Feda.local>
2026-04-06 19:29:02 +0200

    feat: подключил запросы на страницу sberbank deposit

--- src/entities/sberbank-deposit-intro/use-sberbank-deposit-intro.ts
+++ src/entities/sberbank-deposit-intro/use-sberbank-deposit-intro.ts
@@ -3,13 +3,15 @@ import { useQuery } from 'react-query';
 import { QUERY_KEYS } from '../query-keys';
 import type { TSberbankDepositIntroData } from './types';
 
-import { MOCK_SBERBANK_DEPOSIT_INTRO } from './__mocks__';
+import { api } from '../../shared/api';
 
 export const useSberbankDepositIntro = () => {
   return useQuery<TSberbankDepositIntroData>({
     queryKey: [QUERY_KEYS.sberbankDepositIntro],
     queryFn: async () => {
-      return MOCK_SBERBANK_DEPOSIT_INTRO;
+      const res = await api.get('/api/project-pages/intro/by-slug/deposit');
+
+      return res.data;
     },
   });
 };
--- src/entities/sberbank-deposit-points/use-sberbank-deposit-points.ts
+++ src/entities/sberbank-deposit-points/use-sberbank-deposit-points.ts
@@ -2,13 +2,17 @@ import { useQuery } from 'react-query';
 
 import { QUERY_KEYS } from '../query-keys';
 import type { TSberbankDepositPointsData } from './types';
-import { MOCK_SBERBANK_DEPOSIT_POINTS } from './__mocks__';
+import { api } from '../../shared/api';
 
 export const useSberbankDepositPoints = () => {
   return useQuery<TSberbankDepositPointsData[]>({
     queryKey: [QUERY_KEYS.sberbankDepositPoints],
     queryFn: async () => {
-      return MOCK_SBERBANK_DEPOSIT_POINTS;
+      const res = await api.get(
+        '/api/project-pages/seller-hub/by-slug/sberbank-deposit',
+      );
+
+      return res.data;
     },
   });
 };
