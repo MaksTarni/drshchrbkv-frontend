ec81ca6fe6e5c9981e0500a47074e9238a226265
FVS <fvs@MacBook-Pro-Feda.local>
2026-05-19 20:34:04 +0200

    fix: правки фотографий

--- src/features/sberbank-metal-introducing/sberbank-metal-introducing-connector.tsx
+++ src/features/sberbank-metal-introducing/sberbank-metal-introducing-connector.tsx
@@ -2,15 +2,30 @@ import { useMemo } from 'react';
 import { SberbankMetalIntroducing } from './sberbank-metal-introducing';
 import { useSberbankMetalIntro } from '../../entities/sberbank-metal-intro';
 import { mapDataToUI } from './utils';
+import { resolveImageByBreakpoint } from '../../shared/utils';
+import { useDimensions } from '../../shared/hooks/dimensions';
 
 export const SberbankMetalIntroducingConnector = () => {
+  const breakpoints = useDimensions();
   const { data, isLoading } = useSberbankMetalIntro();
 
   const screenData = useMemo(() => mapDataToUI(data), [data]);
+
+  const resolvedImage = resolveImageByBreakpoint({
+    defaultImage: screenData?.desktopImage,
+    mobileImage: screenData.mobileImage,
+    tabletImage: screenData.tabletImage,
+    breakpoints,
+  });
 
   if (isLoading) {
     return null;
   }
 
-  return <SberbankMetalIntroducing screenData={screenData} />;
+  return (
+    <SberbankMetalIntroducing
+      screenData={screenData}
+      image={resolvedImage}
+    />
+  );
 };
